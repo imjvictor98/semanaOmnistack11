8222caaf15dd71315cf09c98625d3089147c508b
imjvictor98 <jv.camara@hotmail.com>
2020-03-26 19:54:37 -0300

    Config padrão do react native

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+import React from 'react'
 import { Text, View } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+      <Routes/>
   );
 }
 
